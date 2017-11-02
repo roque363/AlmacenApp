b33e8faf4c6db627c9549b8d57d8e26293f09b45
Roque Alarcon <roquealarcon363@gmail.com>
2017-11-02 12:38:22 -0500

    Initial commit

---
+++ app/src/main/java/com/roque/almacenapp/model/ResponseMessage.java
@@ -0,0 +1,36 @@
+package com.roque.almacenapp.model;
+
+/**
+ * Created by ROQUE on 1/11/2017.
+ */
+
+public class ResponseMessage {
+
+    private String type;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseMessage{" +
+                "type='" + type + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+}

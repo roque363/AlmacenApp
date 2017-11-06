2c51e446bb1fa609910fe3aafa0d60f2591f75a8
Roque Alarcon <roquealarcon363@gmail.com>
2017-11-06 12:21:08 -0500

    mejor diseño

---
+++ app/src/main/java/com/roque/almacenapp/activities/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.roque.almacenapp.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roque.almacenapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000); // 3 segundos
+
+    }
+}
